b183e5d642c391d60443eeff9cdb8a7cf32ef759
Suran Warnakulasooriya <68828123+suranwarnakulasooriya@users.noreply.github.com>
2024-03-19 10:11:55 -0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -15,6 +15,7 @@ target_fps = 20 # target frames per second
 def init_dimens(screen:curses.window) -> (bool,int,int): # get dimensions of game from terminal
     h,w = screen.getmaxyx()[0]-2,screen.getmaxyx()[1]-2; f = True
     if w < 25 or h < 25: f = False # the screen is not functional if the dimensions are too small
+    #w = h
     return f,w,h # return whether the screen is big enough and dimens
 
 def generate_apple(s:list[(int,int)],a:int,b:int) -> (int,int): 
@@ -32,13 +33,17 @@ def close(score:int,highscore:int,stdscr:curses.window) -> None:
 if __name__ == '__main__':
 
     # i/o setup
-    stdscr = curses.initscr()
+    fullscr = curses.initscr()
     curses.start_color(); curses.use_default_colors() # use ansi colors
-    curses.cbreak(); curses.noecho(); stdscr.nodelay(True) # make getch() nonblocking
-    curses.curs_set(0); stdscr.keypad(True) # hide mouse and allow keyboard input
+    #curses.cbreak(); curses.noecho(); fullscr.nodelay(True) # make getch() nonblocking
+    #curses.curs_set(0); fullscr.keypad(True) # hide mouse and allow keyboard input
     target_frametime = 1/target_fps # target time per frame in seconds
 
-    f,w,h = init_dimens(stdscr) # get dimens
+    f,w,h = init_dimens(fullscr) # get dimens
+    stdscr = fullscr.subwin(h+2,w+2,0,0)
+    curses.cbreak(); curses.noecho(); stdscr.nodelay(True)
+    curses.curs_set(0); stdscr.keypad(True)
+
     grid = [[0]*w for _ in range(h)] # empty grid
     tui_color = True # game starts with colors
 
@@ -63,10 +68,12 @@ while __name__ == '__main__':
     try:
         # INPUT ===================================================================================
 
+        
         dt1 = datetime.now() # get current time
         input_char = stdscr.getch(); stdscr.erase() # get input and clear screen
 
         if input_char == curses.KEY_RESIZE: # reset game if window is resized
+            curses.resize_term(30,30)
             f,w,h = init_dimens(stdscr); # get new dimens
             snake = [(0,0),(0,1),(0,2),(0,3)]; apple = generate_apple(snake, w, h)
             d = 'r'; arrow = '→'; paused = True; gameover = False; score = 0
@@ -86,28 +93,17 @@ while __name__ == '__main__':
             elif (input_char == curses.KEY_LEFT) and d != 'r': d = 'l'
             elif (input_char == curses.KEY_UP) and d != 'd': d = 'u'
             elif (input_char == curses.KEY_DOWN) and d != 'u': d = 'd'
-            head = snake[-1] # the snake's head is the last index
+            head = [snake[-1][0],snake[-1][1]] # the snake's head is the last index
 
         # GAME LOGIC ==============================================================================
 
             # increase length of snake in current direction
-            if d == 'r':
-                arrow = '→'
-                if head[1] == w-1: snake.append((head[0],0))
-                else: snake.append((head[0],head[1]+1))
-            elif d == 'l':
-                arrow = '←'
-                if head[1] == 0: snake.append((head[0],w-1))
-                else: snake.append((head[0],head[1]-1))
-            elif d == 'u':
-                arrow = '↑'
-                if head[0] == 0: snake.append((h-1,head[1]))
-                else: snake.append((head[0]-1,head[1]))
-            else:
-                arrow = '↓'
-                if head[0] == h-1: snake.append((0,head[1]))
-                else: snake.append((head[0]+1,head[1]))
+            if d == 'r':   arrow = '→'; head[1] += 1
+            elif d == 'l': arrow = '←'; head[1] -= 1
+            elif d == 'u': arrow = '↑'; head[0] -= 1
+            else:          arrow = '↓'; head[0] += 1
             
+            snake.append((head[0]%h,head[1]%w))
             # if the snake has reached the apple, generate a new apple and let the snake grow
             if snake[-1] == apple:
                 apple = generate_apple(snake, w, h); score += 1
@@ -128,18 +124,18 @@ while __name__ == '__main__':
             # draw apple
             stdscr.addstr(apple[0]+1,apple[1]+1,'',curses.color_pair(color_map[tui_color][0]))
             for block in snake: # draw snake
-                stdscr.addstr(block[0]+1,block[1]+1,'█',curses.color_pair(color_map[tui_color][1]))
-            stdscr.addstr(0,1,f' SCORE: {score} ') # score text
-            stdscr.addstr(h+1,1,f' HIGH SCORE: {highscore} ') # high score text
+                stdscr.addstr(block[0]+1,block[1]+1,'█',curses.color_pair(color_map[tui_color][not gameover]))
+            stdscr.addstr(0,1,f' {score} ') # score text
+            stdscr.addstr(h+1,1,f' {highscore} ') # high score text
             dims = f' {w}x{h} '; stdscr.addstr(h+1,w+1-len(dims),dims) # show dimens
-            if gameover: stdscr.addstr(0,w-13,' R TO RESTART ') # restart text
+            if gameover: stdscr.addstr(0,w-12,'R TO RESTART') # restart text
             elif paused:
                 stdscr.addstr(snake[-1][0]+1,snake[-1][1]+1,arrow, # draw arrow to show snake...
                 curses.color_pair(color_map[tui_color][1])) # ...direction when paused
-                stdscr.addstr(0,w-7,' PAUSED ') # paused text
+                stdscr.addstr(0,w-6,'PAUSED') # paused text
         else: stdscr.addstr('Window is not big enough (need at least 25x25).') # error text
 
         sleep(max(0,target_frametime-(datetime.now()-dt1).microseconds/1e6)) # maintain frame rate
         stdscr.refresh()
 
-    except KeyboardInterrupt: close(score,highscore,stdscr) # quit on ^C
+except KeyboardInterrupt: close(score,highscore,stdscr) # quit on ^C
